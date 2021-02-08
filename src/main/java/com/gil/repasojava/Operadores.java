8ea8c875e29d77b98f067d5af21d68ffe124c0c8
Gil DO <gilbertodemetrio@yahoo.com>
2021-02-07 19:20:32 -0600

    Curso de Java 8

---
+++ src/main/java/com/gil/repasojava/Operadores.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gil.repasojava;
+
+/**
+ *
+ * @author Dell
+ */
+public class Operadores {
+    
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        
+    }
+}
